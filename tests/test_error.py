c4fb5f876bf2bcb2fe35e48452527603bb20bdd5
Nick Frost <nickfrostatx@gmail.com>
2015-11-12 01:31:45 -0800

    Use with for app test client

--- tests/test_error.py
+++ tests/test_error.py
@@ -8,7 +8,6 @@ import werkzeug.exceptions
 
 def test_custom_handler():
     app = flask.Flask(__name__)
-    client = app.test_client()
 
     def handler(e):
         return e.name + '\n', e.code
@@ -31,25 +30,25 @@ def test_custom_handler():
     def divide_by_zero():
         1/0
 
-    rv = client.get('/good')
-    assert rv.status_code == 200
+    with app.test_client() as client:
+        rv = client.get('/good')
+        assert rv.status_code == 200
 
-    rv = client.get('/418')
-    assert rv.data == b'I\'m a teapot\n'
-    assert rv.status_code == 418
+        rv = client.get('/418')
+        assert rv.data == b'I\'m a teapot\n'
+        assert rv.status_code == 418
 
-    rv = client.get('/500')
-    assert rv.data == b'Internal Server Error\n'
-    assert rv.status_code == 500
+        rv = client.get('/500')
+        assert rv.data == b'Internal Server Error\n'
+        assert rv.status_code == 500
 
-    rv = client.get('/internal')
-    assert rv.data == b'Internal Server Error\n'
-    assert rv.status_code == 500
+        rv = client.get('/internal')
+        assert rv.data == b'Internal Server Error\n'
+        assert rv.status_code == 500
 
 
 def test_html_handler():
     app = flask.Flask(__name__, template_folder='../frost/templates')
-    client = app.test_client()
     register_error_handler(app, html_handler)
 
     @app.route('/good')
@@ -64,18 +63,19 @@ def test_html_handler():
     def divide_by_zero():
         1/0
 
-    rv = client.get('/good')
-    assert rv.status_code == 200
+    with app.test_client() as client:
+        rv = client.get('/good')
+        assert rv.status_code == 200
 
-    rv = client.get('/418')
-    assert b'<title>Frost CI - I&#39;m a teapot</title>' in rv.data
-    assert b'<h1>Error 418</h1>' in rv.data
-    assert rv.status_code == 418
+        rv = client.get('/418')
+        assert b'<title>Frost CI - I&#39;m a teapot</title>' in rv.data
+        assert b'<h1>Error 418</h1>' in rv.data
+        assert rv.status_code == 418
 
-    rv = client.get('/internal')
-    assert b'<title>Frost CI - Internal Server Error</title>' in rv.data
-    assert b'<h1>Error 500</h1>' in rv.data
-    assert rv.status_code == 500
+        rv = client.get('/internal')
+        assert b'<title>Frost CI - Internal Server Error</title>' in rv.data
+        assert b'<h1>Error 500</h1>' in rv.data
+        assert rv.status_code == 500
 
 
 def test_not_500():
