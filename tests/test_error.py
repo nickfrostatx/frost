437f9543dde75a7ce131011ccc041f8ede8a6838
Nick Frost <nickfrostatx@gmail.com>
2015-11-12 17:58:25 -0800

    Test app and blueprint error handler separately

--- tests/test_error.py
+++ tests/test_error.py
@@ -47,6 +47,54 @@ def test_custom_handler():
         assert rv.status_code == 500
 
 
+def test_blueprint_handler():
+    app = flask.Flask(__name__)
+    bp = flask.Blueprint('bp', __name__)
+
+    def app_handler(e):
+        return 'App: ' + e.name + '\n', e.code
+    register_error_handler(app, app_handler)
+
+    def bp_handler(e):
+        return 'Blueprint: ' + e.name + '\n', e.code
+    register_error_handler(bp, bp_handler)
+
+    @bp.route('/good')
+    def good():
+        return 'OK'
+
+    @bp.route('/418')
+    def teapot():
+        flask.abort(418)
+
+    @bp.route('/500')
+    def internal():
+        raise werkzeug.exceptions.InternalServerError()
+
+    @bp.route('/internal')
+    def divide_by_zero():
+        1/0
+
+    app.register_blueprint(bp)
+
+    with app.test_client() as client:
+        rv = client.get('/good')
+        assert rv.data == b'OK'
+        assert rv.status_code == 200
+
+        rv = client.get('/418')
+        assert rv.data == b'Blueprint: I\'m a teapot\n'
+        assert rv.status_code == 418
+
+        rv = client.get('/500')
+        assert rv.data == b'App: Internal Server Error\n'
+        assert rv.status_code == 500
+
+        rv = client.get('/internal')
+        assert rv.data == b'App: Internal Server Error\n'
+        assert rv.status_code == 500
+
+
 def test_html_handler():
     app = flask.Flask(__name__, template_folder='../frost/templates')
     register_error_handler(app, html_handler)
@@ -75,24 +123,4 @@ def test_html_handler():
         rv = client.get('/internal')
         assert b'<title>Frost CI - Internal Server Error</title>' in rv.data
         assert b'<h1>Error 500</h1>' in rv.data
-        assert rv.status_code == 500
-
-
-def test_not_500():
-    app = flask.Flask(__name__)
-    bp = flask.Blueprint('bp', __name__)
-
-    def handler(e):
-        return e.name + '\n', e.code
-    register_error_handler(bp, handler)
-
-    @bp.route('/internal')
-    def divide_by_zero():
-        1/0
-
-    app.register_blueprint(bp)
-
-    with app.test_client() as client:
-        rv = client.get('/internal')
-        assert b'<!DOCTYPE HTML PUBLIC' in rv.data
         assert rv.status_code == 500
