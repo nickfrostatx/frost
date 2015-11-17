eefb063ba8e8499613236b932bea2400d881070b
Nick Frost <nickfrostatx@gmail.com>
2015-11-16 22:17:27 -0800

    Move serving_app into utility file

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,34 +1,11 @@
 # -*- coding: utf-8 -*-
 """Test the GitHub API."""
 
-from werkzeug.serving import ThreadedWSGIServer
+from util import serving_app
 import flask
 import frost.exceptions
 import frost.github
 import pytest
-import random
-import threading
-
-
-@pytest.fixture
-def serving_app(request):
-    """Create a Flask app and serve it over some port 8xxx.
-
-    This will be used to pretend to be GitHub, so we can see test what
-    happens when GitHub returns unexpected responses. When the test
-    ends, the server is destroyed.
-    """
-    host = '127.0.0.1'
-    port = random.randint(8000, 8999)
-    app = flask.Flask(__name__)
-    app.debug = True
-
-    app.url = 'http://{0}:{1}'.format(host, port)
-    server = ThreadedWSGIServer(host, port, app)
-    thread = threading.Thread(target=server.serve_forever)
-    thread.start()
-    request.addfinalizer(server.shutdown)
-    return app
 
 
 @pytest.fixture
---
+++ tests/util.py
@@ -0,0 +1,29 @@
+## -*- coding: utf-8 -*-
+"""Shared utility functions."""
+
+from werkzeug.serving import ThreadedWSGIServer
+import flask
+import pytest
+import random
+import threading
+
+
+@pytest.fixture
+def serving_app(request):
+    """Create a Flask app and serve it over some port 8xxx.
+
+    This will be used to pretend to be GitHub, so we can see test what
+    happens when GitHub returns unexpected responses. When the test
+    ends, the server is destroyed.
+    """
+    host = '127.0.0.1'
+    port = random.randint(8000, 8999)
+    app = flask.Flask(__name__)
+    app.debug = True
+
+    app.url = 'http://{0}:{1}'.format(host, port)
+    server = ThreadedWSGIServer(host, port, app)
+    thread = threading.Thread(target=server.serve_forever)
+    thread.start()
+    request.addfinalizer(server.shutdown)
+    return app
