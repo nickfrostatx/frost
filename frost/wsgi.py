fae724c269b6741bed7a62ea24dc49e9b68a2105
Nick Frost <nickfrostatx@gmail.com>
2015-11-18 17:19:05 -0800

    Reame wsgi.app to the standard wsgi.application

--- frost/wsgi.py
+++ frost/wsgi.py
@@ -4,4 +4,4 @@
 from .app import create_app
 
 
-app = create_app()
+application = create_app()
--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -9,8 +9,8 @@ def test_wsgi_app_config():
     os.environ['REDIS_URL'] = 'redis://user:pass@example.com/2'
     os.environ['GITHUB_CLIENT_ID'] = 'my client id'
     os.environ['GITHUB_CLIENT_SECRET'] = 'my client secret'
-    from frost.wsgi import app
-    assert isinstance(app, flask.Flask)
-    assert app.config['REDIS_URL'] == 'redis://user:pass@example.com/2'
-    assert app.config['GITHUB_CLIENT_ID'] == 'my client id'
-    assert app.config['GITHUB_CLIENT_SECRET'] == 'my client secret'
+    from frost.wsgi import application
+    assert isinstance(application, flask.Flask)
+    assert application.config['REDIS_URL'] == 'redis://user:pass@example.com/2'
+    assert application.config['GITHUB_CLIENT_ID'] == 'my client id'
+    assert application.config['GITHUB_CLIENT_SECRET'] == 'my client secret'
