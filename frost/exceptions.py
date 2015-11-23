c781b2abfcc8f04e923afddb3961c25f38250a42
Nick Frost <nickfrostatx@gmail.com>
2015-11-22 16:43:13 -0800

    Remove GitHub exception

--- frost/exceptions.py
+++ frost/exceptions.py
@@ -15,24 +15,3 @@ class NoSuchUserException(Exception):
 class NoSuchRepoException(Exception):
 
     """The user has no repo with the given name."""
-
-
-class GitHubError(Exception):
-
-    """Communication with the GitHub API has failed."""
-
-    def __str__(self):
-        """Return the error message, or a generic message."""
-        try:
-            message = '{0}: {1}'.format(self.response.status_code,
-                                        self.response.json()['message'])
-        except Exception:
-            message = 'Failed to communicate with GitHub'
-        return message
-
-    @property
-    def response(self):
-        """The :class:`~requests.Response` object for the request."""
-        if len(self.args) < 1:
-            return None
-        return self.args[0]
