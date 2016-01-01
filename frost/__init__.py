c1a3d24023ad93559a47196e6a4382878d991c9c
Nick Frost <nickfrostatx@gmail.com>
2016-01-01 16:55:14 -0600

    Happy New Year! :sparkles:

--- frost/__init__.py
+++ frost/__init__.py
@@ -2,12 +2,12 @@
 """
 Totally not a Travis clone.
 
-:copyright: (c) 2015 by Nicholas Frost.
+:copyright: (c) 2016 by Nicholas Frost.
 :license: MIT, see LICENSE for more details.
 """
 
 __author__ = 'Nick Frost'
-__copyright__ = 'Copyright 2015, Nicholas Frost'
+__copyright__ = 'Copyright 2016, Nicholas Frost'
 __license__ = 'MIT'
 __version__ = '0.1.0'
 __email__ = 'nickfrostatx@gmail.com'
