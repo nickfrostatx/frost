c6d62d8478425cf20339b13605713cabbf4b2a10
Nick Frost <nickfrostatx@gmail.com>
2016-01-01 17:04:42 -0600

    Don't run an error handler on hooks

--- frost/hooks.py
+++ frost/hooks.py
@@ -2,15 +2,9 @@
 """GitHub web hooks."""
 
 from flask.ext.hookserver import Hooks
-from .error import errorhandler
 
 
 hooks = Hooks()
-
-
-@errorhandler(hooks)
-def error(e):
-    return 'hwhat\n', e.code
 
 
 @hooks.hook('ping')
