cd4a00e4cd6d2ba1f69b717a0f018611b4c170ce
Nick Frost <nickfrostatx@gmail.com>
2016-07-25 20:04:56 -0700

    Use file-based config

--- frost/app.py
+++ frost/app.py
@@ -3,16 +3,18 @@
 
 from flask import Flask
 from . import __name__ as package_name
-import os
 
 
 def create_app():
     """Return an instance of the main Flask application."""
     app = Flask(package_name)
 
-    for var in ('GITHUB_CLIENT_ID', 'GITHUB_CLIENT_SECRET', 'REDIS_URL',
-                'GITHUB_WEBHOOKS_KEY'):
-        app.config.setdefault(var, os.environ.get(var, ''))
+    app.config.from_envvar('CONFIG_PATH', silent=True)
+
+    app.config.setdefault('GITHUB_CLIENT_ID', '')
+    app.config.setdefault('GITHUB_CLIENT_SECRET', '')
+    app.config.setdefault('GITHUB_WEBHOOKS_KEY', '')
+    app.config.setdefault('REDIS_URL', '')
 
     from .error import register_error_handler, html_handler
     register_error_handler(app, html_handler)
--- tests/test_app.py
+++ tests/test_app.py
@@ -15,12 +15,19 @@ def test_create_app():
     assert app.config['GITHUB_CLIENT_SECRET'] == ''
 
 
-def test_app_config():
-    os.environ['REDIS_URL'] = 'redis://user:pass@example.com/2'
-    os.environ['GITHUB_CLIENT_ID'] = 'my client id'
-    os.environ['GITHUB_CLIENT_SECRET'] = 'my client secret'
+def test_app_config(tmpdir):
+    f = tmpdir.join('config.py')
+    f.write('''
+REDIS_URL = 'redis://user:pass@example.com/2'
+GITHUB_CLIENT_ID = 'my client id'
+GITHUB_CLIENT_SECRET = 'my client secret'
+''')
+    os.environ.clear()
+    os.environ['CONFIG_PATH'] = str(f)
+
     app = frost.app.create_app()
     assert isinstance(app, flask.Flask)
     assert app.config['REDIS_URL'] == 'redis://user:pass@example.com/2'
     assert app.config['GITHUB_CLIENT_ID'] == 'my client id'
     assert app.config['GITHUB_CLIENT_SECRET'] == 'my client secret'
+    assert app.config['GITHUB_WEBHOOKS_KEY'] == ''
